efba66be7746c43425485ea91d4aad1e45c207f3
Nathan Schwartz <31522523+SchwartzNathan99@users.noreply.github.com>
2019-09-06 21:44:48 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require('http'), 
+    fs = require('fs'), 
+    url = require('url'),
+    port = 8080;
+
+/* Global variables */
+var listingData, server;
+
+var requestHandler = function(request, response) {
+  var parsedUrl = url.parse(request.url);
+
+    if (parsedUrl.path == '/listings')
+    {
+        response.write(listingData);
+  	response.end();
+    } 
+    else
+    {
+  	response.writeHead(404);
+  	response.write('Bad gateway error');
+  	response.end();
+    }
+    
+};
+
+var server = http.createServer(requestHandler);
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+     if (err) throw err;
+     listingData = data;
+     
+     server.listen(port, function() 
+     {
+  	console.log('Server listening on: http://127.0.0.1:' + port);
+     });
+
+});
